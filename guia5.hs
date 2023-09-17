13b2495fc6b44129e5a206b0f5ae86779bdac6eb
Rodrigo Ladron de Guevara <ro.ladrondeguevara@gmail.com>
2023-09-16 21:46:20 -0300

    added guia5 ejs

--- guia5.hs
+++ guia5.hs
@@ -1,14 +1,68 @@
---ej 2.4   
-hayRepetidos :: (Eq t) => [t] -> Bool
-hayRepetidos [] = False
-hayRepetidos (x:xs) | pertenece x xs = True
-                      | otherwise = hayRepetidos xs
+-- 1.1
+longitud :: [t] -> Integer
+longitud [] = 0
+longitud (x:xs) = 1 + longitud xs
 
+--1.2
+ultimo :: [t] -> t
+ultimo (x:xs) | longitud (x:xs) == 1 = x
+              | otherwise = ultimo xs
+
+--1.3
+principio :: [t] -> [t]
+principio (x:xs) | longitud (x:xs) == 1 = []
+                 | otherwise = x : principio xs
+--1.4
+reverso :: [t] -> [t]
+reverso t | longitud t == 0 = []
+          | otherwise = (ultimo t) : reverso (principio t)
+
+--2.1
 pertenece :: (Eq t) => t -> [t] -> Bool
 pertenece n [] = False
 pertenece n (x:xs) | n == x = True
                    | otherwise = pertenece n xs
 
+--2.2
+todosIguales :: (Eq t) => [t] -> Bool
+todosIguales (x:xs) | longitud xs == 0 = True
+                    | x /= head xs = False
+                    | otherwise = todosIguales xs
+
+--2.3
+todosDistintos :: (Eq t) => [t] -> Bool
+todosDistintos (x:xs) | longitud xs == 0 = True
+                      | pertenece x xs = False
+                      | otherwise = todosDistintos xs
+
+--ej 2.4
+hayRepetidos :: (Eq t) => [t] -> Bool
+hayRepetidos [] = False
+hayRepetidos (x:xs) | pertenece x xs = True
+                    | otherwise = hayRepetidos xs
+
+--2.5
+quitar :: (Eq t) => t -> [t] -> [t]
+quitar x [] = []
+quitar x xs | x == head xs = [] ++ tail xs
+            | otherwise = [head xs] ++ quitar x (tail xs)
+
+-- 2.6
+quitarTodos :: (Eq t ) => t -> [t] -> [t]
+quitarTodos n [] = []
+quitarTodos n xs | n == head xs = [] ++ quitarTodos n (tail xs)
+                 | otherwise = [head xs] ++ quitarTodos n (tail xs)
+
+-- 2.7
+eliminarRepetidos :: (Eq t) => [t] -> [t]
+eliminarRepetidos [] = []
+eliminarRepetidos (x:xs) | pertenece x xs = x : quitarTodos x (eliminarRepetidos xs)
+                         | otherwise = x : eliminarRepetidos xs
+-- 2.8
+mismosElementos :: (Eq t) => [t] -> [t] -> Bool
+mismosElementos (x:xs) (y:ys) | x == y && xs == [] && ys == [] = True
+                              | pertenece x (y:ys) && pertenece y (x:xs) = mismosElementos xs ys
+                              | otherwise = False
 --ej 3
 maximo :: [Int] -> Int
 maximo [x] = x
